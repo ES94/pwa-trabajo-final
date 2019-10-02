d72fec98d1743264c1f85c842b33fe4e5b6deab8
Antares94 <e.stradella94@outlook.com>
2019-10-02 01:28:25 -0300

    Integrar con Bootstrap y Firebase

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="sm" type="dark" variant="dark">
+      <b-container>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#">DIRSA</b-navbar-brand>
+        <b-collapse is-nav id="nav-collapse">
+          <b-navbar-nav vertical>
+            <b-nav-item to="/">Inicio</b-nav-item>
+            <b-nav-item to="/about">Acerca</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+    <router-view class="text-center" />
+  </div>
+</template>
