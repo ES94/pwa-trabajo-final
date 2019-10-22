1e7137aec2f8f07d92a52647ce5a5233c045b14d
Antares94 <e.stradella94@outlook.com>
2019-10-22 10:28:41 -0300

    Insertar menú de botones en la vista de inicio y eliminar componente HelloWorld

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <b-container class="mt-3 mb-3">
+      <b-row align-h="center" align-v="center">
+        <b-col sm="8" md="6">
+          <b-card border-variant="dark" header="Elija una opción">
+            <b-list-group flush>
+              <b-list-group-item>
+                <b-button variant="primary">Añadir nuevo tambo</b-button>
+              </b-list-group-item>
+              <b-list-group-item>
+                <b-button variant="primary">Elegir tambo</b-button>
+              </b-list-group-item>
+            </b-list-group>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
