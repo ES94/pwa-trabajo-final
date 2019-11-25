934c6dcbb9c0a2da2f9da0a8bb9b27d6593bbb1d
Antares94 <e.stradella94@outlook.com>
2019-11-25 20:29:34 -0300

    Crear componente de edición de tambos

---
+++ src/components/EdicionTambo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-container>
+      <!-- Título del cuadro modal -->
+      <b-row>
+        <b-col>
+            <!-- Puede ser "creación" o "edición" -->
+            {{tipoEdicion}} de tambo
+        </b-col>
+      </b-row>
+
+      <!-- Contenido del modal -->
+      <b-row>
+          <b-col>
+
+          </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "edicionTambo",
+    props: {
+        tipoEdicion: String
+    }
+}
+</script>
